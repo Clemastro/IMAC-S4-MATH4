92868e0f3cc60fd240cac0cd18e1ad081d78e222
Adam <adamferreira@protonmail.com>
2022-06-01 15:56:32 +0200

    Correction fonction exponentielle

--- src/Random/Exponentielle.cpp
+++ src/Random/Exponentielle.cpp
@@ -7,7 +7,7 @@
 double exponentielle(float p)
 {
     double res = -log(random_uniform_int(0, 100) / 100.0) / p;
-    if (isinfl(res)) {
+    if (isinf(res)) {
         res = 0;
     }
     return res;
